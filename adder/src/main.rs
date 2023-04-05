571ed4429117d35c9a4df2645d3868d8efded0db
Aaron Li <a1li@ucsd.edu>
2023-04-05 09:56:59 -0700

    Add simple "compiler"

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -1,3 +1,11 @@
+/// Compile a source program into a string of x86-64 assembly code.
+fn compile(program: String) -> String {
+    let num = program.trim().parse::<i32>().unwrap();
+    return format!("mov rax, {}", num);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let program = String::from("37");
+    let assembly = compile(program);
+    println!("{}", assembly);
 }
