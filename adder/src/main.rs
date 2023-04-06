88e4e51ead7a833d66429aeed550331cc935ea6c
Aaron Li <a1li@ucsd.edu>
2023-04-06 09:41:13 -0700

    More complicated main function to read/write to files

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -1,11 +1,35 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 /// Compile a source program into a string of x86-64 assembly code.
 fn compile(program: String) -> String {
     let num = program.trim().parse::<i32>().unwrap();
     return format!("mov rax, {}", num);
 }
 
-fn main() {
-    let program = String::from("37");
-    let assembly = compile(program);
-    println!("{}", assembly);
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let in_name = &args[1];
+    let out_name = &args[2];
+
+    let mut in_file = File::open(in_name)?;
+    let mut in_contents = String::new();
+    in_file.read_to_string(&mut in_contents)?;
+
+    let result = compile(in_contents);
+
+    let asm_program = format!("
+section .text
+global our_code_starts_here
+our_code_starts_here:
+  {}
+  ret
+", result);
+
+    let mut out_file = File::create(out_name)?;
+    out_file.write_all(asm_program.as_bytes())?;
+
+    Ok(())
 }
